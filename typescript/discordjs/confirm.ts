8cd401fcd0a96bf252987f5bf379cd1c33db3fc0
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-10-14 21:36:25 +0200

    properly updted confirm snippet to new discord.js version

--- typescript/discordjs/confirm.ts
+++ typescript/discordjs/confirm.ts
@@ -1,108 +1,108 @@
 import {
-  ButtonInteraction,
-  CommandInteraction,
-  InteractionReplyOptions,
-  Message,
-  ActionRowBuilder,
-  ButtonBuilder,
-  MessageOptions,
-  ButtonStyle,
-} from "discord.js";
+	ButtonInteraction,
+	CommandInteraction,
+	InteractionReplyOptions,
+	Message,
+	ActionRowBuilder,
+	ButtonBuilder,
+	BaseMessageOptions,
+	ButtonStyle,
+} from 'discord.js';
 
 interface ConfirmOptions {
-  redBtnText: string;
-  greenBtnText: string;
-  context: Message | CommandInteraction;
-  onConfirm: (interaction: ButtonInteraction) => void;
-  onDecline: (Interaction: ButtonInteraction) => void;
+	redBtnText: string;
+	greenBtnText: string;
+	context: Message | CommandInteraction;
+	onConfirm: (interaction: ButtonInteraction) => void;
+	onDecline: (Interaction: ButtonInteraction) => void;
 }
 
 export class Confirm {
-  private row: ActionRowBuilder;
-  private context: Message | CommandInteraction;
-  private onConfirm: (interaction: ButtonInteraction) => void;
-  private onDecline: (interaction: ButtonInteraction) => void;
-  private greenBtnText: string;
-  private redBtnText: string;
-  constructor({
-    onConfirm,
-    onDecline,
-    context,
-    greenBtnText,
-    redBtnText,
-  }: ConfirmOptions) {
-    this.redBtnText = redBtnText;
-    this.greenBtnText = greenBtnText;
-    this.row = new ActionRowBuilder().addComponents([
-      new ButtonBuilder()
-        .setStyle(ButtonStyle.Success)
-        .setLabel(this.greenBtnText)
-        .setCustomId("yes"),
-      new ButtonBuilder()
-        .setStyle(ButtonStyle.Danger)
-        .setLabel(this.redBtnText)
-        .setCustomId("no"),
-    ]);
-    this.context = context;
-    this.onConfirm = onConfirm;
-    this.onDecline = onDecline;
-  }
-  public async reply(payload: MessageOptions | InteractionReplyOptions) {
-    let components = payload.components
-      ? [this.row, ...payload.components]
-      : [this.row];
-    let msg =
-      this.context instanceof Message
-        ? await this.context.reply({
-            ...(payload as MessageOptions),
-            //@ts-ignore d.js make ur fucking typings work
-            components: components,
-          })
-        : ((await this.context.reply({
-            ...(payload as InteractionReplyOptions),
-            //@ts-ignore d.js make ur fucking typings work
-            components: components,
-            fetchReply: true,
-          })) as Message);
-    this.createCollector(msg);
-  }
-  private createCollector(msg: Message) {
-    let usr =
-      this.context instanceof Message ? this.context.author : this.context.user;
-    let collector = msg.createMessageComponentCollector({
-      filter: (int) => int.user.id == usr.id,
-      time: 15_000,
-      max: 1,
-    });
-    collector.on("collect", (int) => {
-      if (!int.isButton()) return;
-      switch (int.customId) {
-        case "yes": {
-          this.onConfirm(int);
-          break;
-        }
-        case "no": {
-          this.onDecline(int);
-          break;
-        }
-      }
-    });
-    collector.on("end", async (_) => {
-      let row = new ActionRowBuilder().addComponents([
-        new ButtonBuilder()
-          .setStyle(ButtonStyle.Success)
-          .setLabel(this.greenBtnText)
-          .setCustomId("yes")
-          .setDisabled(true),
-        new ButtonBuilder()
-          .setStyle(ButtonStyle.Danger)
-          .setLabel(this.redBtnText)
-          .setCustomId("no")
-          .setDisabled(true),
-      ]);
-      //@ts-ignore d.js make ur fucking typings work
-      msg.components[0] = row;
-      msg.edit({ components: msg.components });
-    });
-  }
+	private row: ActionRowBuilder;
+	private context: Message | CommandInteraction;
+	private onConfirm: (interaction: ButtonInteraction) => void;
+	private onDecline: (interaction: ButtonInteraction) => void;
+	private greenBtnText: string;
+	private redBtnText: string;
+	constructor({
+		onConfirm,
+		onDecline,
+		context,
+		greenBtnText,
+		redBtnText,
+	}: ConfirmOptions) {
+		this.redBtnText = redBtnText;
+		this.greenBtnText = greenBtnText;
+		this.row = new ActionRowBuilder().addComponents([
+			new ButtonBuilder()
+				.setStyle(ButtonStyle.Success)
+				.setLabel(this.greenBtnText)
+				.setCustomId('yes'),
+			new ButtonBuilder()
+				.setStyle(ButtonStyle.Danger)
+				.setLabel(this.redBtnText)
+				.setCustomId('no'),
+		]);
+		this.context = context;
+		this.onConfirm = onConfirm;
+		this.onDecline = onDecline;
+	}
+	public async reply(payload: BaseMessageOptions | InteractionReplyOptions) {
+		let components = payload.components
+			? [this.row, ...payload.components]
+			: [this.row];
+		let msg =
+			this.context instanceof Message
+				? await this.context.reply({
+						...(payload as BaseMessageOptions),
+						//@ts-ignore d.js make ur fucking typings work
+						components: components,
+				  })
+				: ((await this.context.reply({
+						...(payload as InteractionReplyOptions),
+						//@ts-ignore d.js make ur fucking typings work
+						components: components,
+						fetchReply: true,
+				  })) as Message);
+		this.createCollector(msg);
+	}
+	private createCollector(msg: Message) {
+		let usr =
+			this.context instanceof Message ? this.context.author : this.context.user;
+		let collector = msg.createMessageComponentCollector({
+			filter: (int) => int.user.id == usr.id,
+			time: 15_000,
+			max: 1,
+		});
+		collector.on('collect', (int) => {
+			if (!int.isButton()) return;
+			switch (int.customId) {
+				case 'yes': {
+					this.onConfirm(int);
+					break;
+				}
+				case 'no': {
+					this.onDecline(int);
+					break;
+				}
+			}
+		});
+		collector.on('end', async (_) => {
+			let row = new ActionRowBuilder().addComponents([
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Success)
+					.setLabel(this.greenBtnText)
+					.setCustomId('yes')
+					.setDisabled(true),
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Danger)
+					.setLabel(this.redBtnText)
+					.setCustomId('no')
+					.setDisabled(true),
+			]);
+			//@ts-ignore d.js make ur fucking typings work
+			msg.components[0] = row;
+			msg.edit({ components: msg.components });
+		});
+	}
 }
